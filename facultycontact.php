b91e3c12f480978412c07a5eaa3e3c2e7b6b1de1
Arindam <majeearindam06072002@gmail.com>
2020-05-06 08:40:04 +0530

    Faculty contact details uploaded

---
+++ facultycontact.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Faculty Contact
+        </title>
+    </head>
+    <body>
+        <?php require "header.php"  ?>
+
+        <h3>Our Faculty Members</h3>
+        <table>
+        <tr>
+            <td><img height="150px" width="150px" src="uploads/profileimg_default.png"  alt="may"></td>
+            <td>
+                <h5>Prof. Sheli Sheela Chakraborty</h5>
+                <p>
+                   HOD, Department of ETCE <br>
+                   Phone:2203-1297, Email:sheli@gmail.com <br>
+                   Office- T-4-1 , Prayukti Bhavan
+                </p>
+            </td>
+                
+                
+        </tr>
+        <tr>
+            <td><img height="150px" width="150px" src="uploads/profileimg_default.png" alt="">   </td>
+            <td>
+                <h5>Prof.Shudhabindu Roy</h5>
+                <p>
+                   Associate Professor, Department of ETCE <br>
+                   Phone:2203-1297, Email:roy@gmail.com
+                   Office- T-4-1 , Prayukti Bhavan
+                </p>
+            </td>
+        </tr>
+       
+        <tr>
+            <td> <img height="150px" width="150px" src="uploads/profileimg_default.png" alt=""></td>
+            <td>
+                <h5>Prof. Chayanika Bose</h5>
+                <p>
+                   Associate Professor, Department of ETCE <br>
+                   Phone:2203-1299, Email:cb@gmail.com <br>
+                   Office- T-4-1 , Prayukti Bhavan
+                </p>
+            </td>
+        </tr>
+        </table>
+        <?php require "footer.php"  ?>        
+    </body>
+</html>
